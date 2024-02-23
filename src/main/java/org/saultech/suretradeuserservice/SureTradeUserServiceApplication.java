479da08882a1eee3be223c87430e3cc228ac945c
Shadrach Adamu <shadrachadamuul@gmail.com>
2024-02-23 10:44:58 +0100

    modified login to support re-sending otp

--- src/main/java/org/saultech/suretradeuserservice/SureTradeUserServiceApplication.java
+++ src/main/java/org/saultech/suretradeuserservice/SureTradeUserServiceApplication.java
@@ -15,7 +15,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 @EnableWebFlux
 @EnableR2dbcRepositories
 @EnableR2dbcAuditing
-@EnableRedisRepositories
 @EnableRetry
 @EnableTransactionManagement
 @EnableAspectJAutoProxy
